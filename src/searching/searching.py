1ce48b9da346f0b94bb7e77a23001a2d33c781c9
Victoria  Topham <topham.victoria@gmail.com>
2020-06-15 10:08:02 -0700

    got binary search working, tests are passing

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -9,6 +11,18 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    
+    floor = 0
+    ceiling = len(arr) - 1
 
+    while ceiling - floor > 1:
+        midpoint = (ceiling - floor) // 2 + floor
+        if target == arr[midpoint]:
+            return midpoint
+        elif target < arr[midpoint]:
+            ceiling = midpoint
+        elif target > arr[midpoint]:
+            floor = midpoint
+    
 
     return -1  # not found
